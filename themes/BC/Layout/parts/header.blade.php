da3b9a98108b333ef78566704ddc794a64c932ce
HanaAnadolli1 <hana.anadolli@progtelx.com>
2025-10-23 14:40:51 +0200

    Update header.blade.php

--- themes/BC/Layout/parts/header.blade.php
+++ themes/BC/Layout/parts/header.blade.php
@@ -1,7 +1,7 @@
 <div class="bravo_header {{ setting_item('enable_header_sticky', 0) == 1 ? 'has_sticky' : '' }}"
     style="max-height: 50px !important;">
     <div class="{{ $container_class ?? 'container' }}">
-        <div class="content">
+        <div class="content" style="max-height: 50px;">
             <div class="header-left">
                 <a href="{{ url(app_get_locale(false, '/')) }}" class="bravo-logo">
                     @php
